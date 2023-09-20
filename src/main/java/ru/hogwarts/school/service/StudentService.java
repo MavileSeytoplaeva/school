7da3d8c750a9caa4512c9abb7c97f2e3b4cf1089
Мавиле Сейтоплаева <mavile.seitoplaeva@gmail.com>
2023-09-20 12:17:28 +0300

    Добавила счетчик и логгер

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -19,6 +20,8 @@ public class StudentService {
     private final FacultyRepository facultyRepository;
 
     Logger logger = LoggerFactory.getLogger(StudentService.class);
+
+    private Integer count = 0;
 
 
     public StudentService(StudentRepository studentRepository, FacultyRepository facultyRepository) {
@@ -129,26 +132,43 @@ public class StudentService {
 
     public void getAllStudentsForThreads() {
         List<Student> students = studentRepository.findAll();
-        System.out.println(students.get(0));
-        System.out.println(students.get(1));
+        logger.info(students.get(0).toString());
+        logger.info(students.get(1).toString());
 
         new Thread(() -> {
-            System.out.println(students.get(2));
-            System.out.println(students.get(3));
+            logger.info(students.get(2).toString());
+            logger.info(students.get(3).toString());
         }).start();
 
         new Thread(() -> {
-            System.out.println(students.get(4));
-            System.out.println(students.get(5));
+            logger.info(students.get(4).toString());
+            logger.info(students.get(5).toString());
         }).start();
     }
 
     public void syncThreads() {
         synchronized (StudentService.class) {
+            printStudent(0);
+            printStudent(1);
+
+            new Thread(() -> {
+                printStudent(2);
+                printStudent(3);
+            }).start();
+
+            new Thread(() -> {
+                printStudent(4);
+                printStudent(5);
+            }).start();
+
+        }
+    }
+
+    private void printStudent(int num) {
+        synchronized (StudentService.class) {
             List<Student> students = studentRepository.findAll();
-            for (int i = 0; i < 6; i++) {
-                System.out.println(students.get(i));
-            }
+            logger.info(students.get(num).toString() + " Count " + count);
+            count++;
         }
     }
 
